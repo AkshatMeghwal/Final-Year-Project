59a1c57ff6266f8f4c0dc2926c557420742d871d
Akshat <akshatk2003@gmail.com>
2025-02-11 00:13:33 +0530

    gemini_api_calls working

--- api_calls.py
+++ api_calls.py
@@ -1,68 +1,45 @@
-# from google import genai
-# import enum
-
-# class Instrument(enum.Enum):
-#   PERCUSSION = "Percussion"
-#   STRING = "String"
-#   WOODWIND = "Woodwind"
-#   BRASS = "Brass"
-#   KEYBOARD = "Keyboard"
-
-# client = genai.Client(api_key="GEMINI_API_KEY")
-# response = client.models.generate_content(
-#     model='gemini-2.0-flash',
-#     contents='What type of instrument is an oboe?',
-#     config={
-#         'response_mime_type': 'text/x.enum',
-#         'response_schema': Instrument,
-#     },
-# )
-
-# print(response.text)
-# # Woodwind
-
-
-
-# from google import genai
-
-# import enum
-# from pydantic import BaseModel
-
-# class Grade(enum.Enum):
-#     A_PLUS = "a+"
-#     A = "a"
-#     B = "b"
-#     C = "c"
-#     D = "d"
-#     F = "f"
-
-# class Recipe(BaseModel):
-#   recipe_name: str
-#   rating: Grade
-
-# client = genai.Client(api_key="GEMINI_API_KEY")
-# response = client.models.generate_content(
-#     model='gemini-2.0-flash',
-#     contents='List 10 home-baked cookies and give them grades based on tastiness.',
-#     config={
-#         'response_mime_type': 'application/json',
-#         'response_schema': list[Recipe],
-#     },
-# )
-
-# print(response.text)
-# # [{"rating": "a+", "recipe_name": "Classic Chocolate Chip Cookies"}, ...]
-
-
-
-import openai
+import enum
 import constants
+from google import genai
 
-openai.api_key = constants.openai_api_key
-response = openai.Completion.create(
-  engine="text-davinci-003",
-  prompt="Tell me a joke",
-  max_tokens=50
+client = genai.Client(api_key=constants.environment_vars.gemini_api_key)
+
+class Instrument(enum.Enum):
+  PERCUSSION = "Percussion"
+  STRING = "String"
+  WOODWIND = "Woodwind"
+  BRASS = "Brass"
+  KEYBOARD = "Keyboard"
+
+# client = genai.Client(api_key="GEMINI_API_KEY")
+response = client.models.generate_content(
+    model='gemini-2.0-flash',
+    contents='What type of instrument is an oboe?',
+    config={
+        'response_mime_type': 'text/x.enum',
+        'response_schema': Instrument,
+    },
 )
+print(response.text)
 
-print(response.choices[0].text.strip())
+# # import constants
+# import os
+# from openai import OpenAI
+
+# client = OpenAI(
+#     api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+# )
+
+# chat_completion = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "Say this is a test",
+#         }
+#     ],
+#     model="gpt-4o",
+# )
+# # try:
+    
+# # except ImportError:
+# #     print("something wrong")
