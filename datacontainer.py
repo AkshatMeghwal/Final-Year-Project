138d52589528421b856b918eced86651cc6840ef
Akshat <akshatk2003@gmail.com>
2025-03-04 03:39:07 +0530

    creating container for js files , constant values for genai

---
+++ datacontainer.py
@@ -0,0 +1,12 @@
+from utils  import Misc
+from os import path
+
+class file_processing_info():
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+        self.codefile_content = Misc.get_content(filepath)
+        self.js_files = []
+        self.src_filepath = ""
+        self.write_filepath = Misc.generate_write_filepath(filepath)
+
+    
