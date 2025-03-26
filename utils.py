86212b2661667bd9725b05becebbbdc2a540f069
Saksham Tayal <tayalsaksham009@gmail.com>
2025-03-27 02:20:02 +0530

    Refactoring code

--- utils.py
+++ utils.py
@@ -122,17 +122,16 @@ class Misc():
             file.write(output)
     
 class CommentRemover:
-    def __init(self):
+    def __init__(self):
         self._JS_LANGUAGE = Language("build/my-languages.so", "javascript")
-        self._parser = Parser()
-    def remove_comments_from_js(js_code):
+        self._parser = Parser(JS_LANGUAGE)
+
+    def remove_comments_from_js(self,js_code):
         """
         Removes comments from the given JavaScript code using Tree-sitter.
         """
-        parser = Parser(JS_LANGUAGE)
-        tree = parser.parse(bytes(js_code, "utf8"))
+        tree = self._parser.parse(bytes(js_code, "utf8"))
         root_node = tree.root_node
-
         def traverse_and_collect(node):
             """
             Recursively traverse the syntax tree and collect non-comment code.
@@ -144,108 +143,3 @@ class CommentRemover:
             return "".join(traverse_and_collect(child) for child in node.children)
 
         return traverse_and_collect(root_node)
-# class CommentRemover:
-#     """Class to handle comment removal"""
-
-#     def __init__(self):
-#         """Initalize and build languages
-
-#         Args:
-#             listoflang (List[str]): list of languages
-#         """
-
-#         # to be editted for genralizing
-#         Language.build_library(
-#             # Store the library in the `build` directory
-#             "build/my-languages.so",
-#             # Include one or more languages
-#             [
-#                 "build/tree-sitter-python",
-#                 "build/tree-sitter-php/php",
-#                 "build/tree-sitter-javascript",
-#             ],
-#         )
-#         self._JS_LANGUAGE = Language("build/my-languages.so", "javascript")
-#         self._lang = {
-#             fte.PYTHON: self._PY_LANGUAGE,
-#             fte.PHP: self._PHP_LANGUAGE,
-#             fte.JAVASCRIPT: self._JS_LANGUAGE,
-#         }
-#         self._commentremover = {
-#             fte.PYTHON: self._remove_commentsfrom_python,
-#             fte.PHP: self._remove_commentsfrom_php,
-#             fte.JAVASCRIPT: self._remove_commentsfrom_javascript,
-#         }
-
-#         # Initialize the parser
-#         self._parser = Parser()
-
-#     def remove_comment(self, code: str, lang: str) -> str:
-#         """Removes comments from the given code based on the specified language.
-
-#         Args:
-#             code (str): The code from which comments need to be removed.
-#             lang (str): The language of the code. Supported languages are determined
-#             by the parser set during initialization.
-#         """
-#         self._parser.set_language(self._lang[lang])
-#         # Parse the code
-#         tree = self._parser.parse(bytes(code, "utf8"))
-#         lines = code.split("\n")
-#         self._commentremover[lang](tree.root_node, lines)
-#         code_without_comments = "\n".join(lines)
-#         code_without_comments = self._clean_the_code(code_without_comments)
-#         return code_without_comments
-
-#     def _clean_the_code(self, text):
-#         # Remove any number of tabs
-#         text = re.sub("\t+", "", text)
-#         # Remove multiple newlines
-#         text = re.sub("\n+", "", text)
-#         # Remove extra spaces
-#         text = re.sub(" +", " ", text)
-#         return text
-
-#     def _remove_commentsfrom_javascript(self, node: Node, lines: List[str]):
-#         """
-#         Removes comments from the given JavaScript code.
-
-#         Args:
-#             node (Node): node to be processed.
-#             lines (List[str]): code lines.
-#         """
-#         if node.type == "comment":
-#             start_line, start_col = node.start_point
-#             end_line, end_col = node.end_point
-
-#             # Handle React.js style comments embedded in brackets
-#             if (
-#                 start_line == end_line
-#                 and lines[start_line][start_col - 1 : start_col + 2] == "{/*"
-#             ):
-#                 lines[start_line] = (
-#                     lines[start_line][: start_col - 1]
-#                     + lines[start_line][end_col + 2 :]
-#                 )
-#             elif start_line != end_line:
-#                 if lines[start_line][start_col - 1 : start_col + 2] == "{/*":
-#                     lines[start_line] = lines[start_line][: start_col - 1]
-#                 if lines[end_line][end_col : end_col + 2] == "*/}":
-#                     lines[end_line] = lines[end_line][end_col + 2 :]
-
-#             # Handle rest js style comments embedded in brackets
-#             if start_line == end_line:  # Single-line comment
-#                 lines[start_line] = (
-#                     lines[start_line][:start_col] + lines[start_line][end_col:]
-#                 )
-#             else:  # Multi-line comment
-#                 lines[start_line] = lines[start_line][:start_col]
-#                 for line_num in range(start_line + 1, end_line):
-#                     lines[line_num] = ""
-#                 lines[end_line] = lines[end_line][end_col:]
-#         elif node.type == "string":
-#             # Strings in JavaScript can contain comments, so no need to remove them
-#             pass
-#         else:
-#             for child in node.children:
-#                 self._remove_commentsfrom_javascript(child, lines)
